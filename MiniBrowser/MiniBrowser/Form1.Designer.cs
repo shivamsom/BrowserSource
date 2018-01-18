1cb774326736cfa3b136d78f25e512b7fbeaece2
shivamsom <shivamsom3@gmail.com>
2018-01-18 20:04:23 +0530

    Source Code of Mini Browser

---
+++ MiniBrowser/MiniBrowser/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace MiniBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.forward = new System.Windows.Forms.Button();
+            this.back = new System.Windows.Forms.Button();
+            this.addressBar = new System.Windows.Forms.TextBox();
+            this.go = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.browser = new System.Windows.Forms.WebBrowser();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 89.75033F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 75F));
+            this.tableLayoutPanel1.Controls.Add(this.forward, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.back, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.addressBar, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.go, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tabControl1, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(1, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.806283F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 93.19372F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1384, 750);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // forward
+            // 
+            this.forward.Location = new System.Drawing.Point(1312, 3);
+            this.forward.Name = "forward";
+            this.forward.Size = new System.Drawing.Size(69, 30);
+            this.forward.TabIndex = 7;
+            this.forward.Text = "Forward";
+            this.forward.UseVisualStyleBackColor = true;
+            this.forward.Click += new System.EventHandler(this.forward_Click);
+            // 
+            // back
+            // 
+            this.back.Location = new System.Drawing.Point(1242, 3);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(64, 30);
+            this.back.TabIndex = 6;
+            this.back.Text = "Back";
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // addressBar
+            // 
+            this.addressBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.addressBar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addressBar.Location = new System.Drawing.Point(3, 3);
+            this.addressBar.Name = "addressBar";
+            this.addressBar.Size = new System.Drawing.Size(1188, 34);
+            this.addressBar.TabIndex = 0;
+            // 
+            // go
+            // 
+            this.go.Location = new System.Drawing.Point(1197, 3);
+            this.go.Name = "go";
+            this.go.Size = new System.Drawing.Size(39, 30);
+            this.go.TabIndex = 5;
+            this.go.Text = "Go";
+            this.go.UseVisualStyleBackColor = true;
+            this.go.Click += new System.EventHandler(this.go_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.tabControl1, 4);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(3, 54);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1378, 693);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.browser);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1370, 664);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "New Tab";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // browser
+            // 
+            this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browser.Location = new System.Drawing.Point(3, 3);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(1364, 658);
+            this.browser.TabIndex = 0;
+            this.browser.Url = new System.Uri("http://www.google.com", System.UriKind.Absolute);
+            this.browser.ProgressChanged += new System.Windows.Forms.WebBrowserProgressChangedEventHandler(this.browser_ProgressChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(1061, 759);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(324, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(1, 756);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 30);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Add Tab";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button2.Location = new System.Drawing.Point(264, 756);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 30);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Close Tab";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button3.Location = new System.Drawing.Point(651, 756);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 30);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Refresh (F5)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1385, 785);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MiniWebBrowser";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox addressBar;
+        private System.Windows.Forms.Button forward;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.Button go;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.WebBrowser browser;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ MiniBrowser/MiniBrowser/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics; //for the Process
+using Microsoft.Win32;  //for the Registry
+namespace MiniBrowser
+{
+    public partial class Form1 : Form
+
+    {
+        public Form1()
+        {
+            var appName = Process.GetCurrentProcess().ProcessName + ".exe";
+            SetIE8KeyforWebBrowserControl(appName);
+            WebBrowserHelper.FixBrowserVersion();
+            WebBrowserHelper.FixBrowserVersion("iexplore.exe");
+            WebBrowserHelper.FixBrowserVersion("iexplore.exe", WebBrowserHelper.GetBrowserVersion());
+           
+            InitializeComponent();
+            browser.ScriptErrorsSuppressed = true;
+         
+        }
+       private void SetIE8KeyforWebBrowserControl(string appName)
+        {
+            RegistryKey Regkey = null;
+            try
+            {
+                // For 64 bit machine
+                if (Environment.Is64BitOperatingSystem)
+                    Regkey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\Wow6432Node\\Microsoft\\Internet Explorer\\MAIN\\FeatureControl\\FEATURE_BROWSER_EMULATION", true);
+                else  //For 32 bit machine
+                    Regkey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION", true);
+
+                // If the path is not correct or
+                // if the user haven't priviledges to access the registry
+                if (Regkey == null)
+                {
+                    MessageBox.Show("Application Settings Failed - Address Not found");
+                    return;
+                }
+
+                string FindAppkey = Convert.ToString(Regkey.GetValue(appName));
+
+                // Check if key is already present
+                if (FindAppkey == "8000")
+                {
+                    MessageBox.Show("Required Application Settings Present");
+                    Regkey.Close();
+                    return;
+                }
+
+                // If a key is not present add the key, Key value 8000 (decimal)
+                if (string.IsNullOrEmpty(FindAppkey))
+                    Regkey.SetValue(appName, unchecked((int)0x1F40), RegistryValueKind.DWord);
+
+                // Check for the key after adding
+                FindAppkey = Convert.ToString(Regkey.GetValue(appName));
+
+                if (FindAppkey == "8000")
+                {    // MessageBox.Show("Application Settings Applied Successfully"); //for testing purpose only.
+
+                }
+                else
+                {
+                    //          MessageBox.Show("Application Settings Failed, Ref: " + FindAppkey); //for testing purpose only.
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Application Settings Failed");
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                // Close the Registry
+                if (Regkey != null)
+                    Regkey.Close();
+            }
+        }
+        private void go_Click(object sender, EventArgs e)
+        {
+            click();    
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            if(browser.CanGoBack)
+            {
+                browser.GoBack();
+            }
+            else { MessageBox.Show("Reached end"); }
+        }
+
+        private void forward_Click(object sender, EventArgs e)
+        {
+            if (browser.CanGoForward)
+            {
+                browser.GoForward();
+            }
+            else { MessageBox.Show("Reached end"); }
+        }
+
+        
+
+        public void click()
+        {
+            if (addressBar.Text != "")//if not empty
+            {
+                tabControl1.SelectedTab.Controls.OfType<WebBrowser>().First().Navigate(addressBar.Text);
+   
+            }
+
+            else
+                MessageBox.Show("Invalid Url");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+      
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            TabPage tab = new TabPage("New Tab");
+            tabControl1.Controls.Add(tab);
+            WebBrowser br = new WebBrowser();
+            br.Parent = tab;
+            br.Dock = DockStyle.Fill;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.RemoveAt(tabControl1.SelectedIndex);
+        }
+
+        private void browser_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            try
+            {
+                progressBar1.Value = Convert.ToInt32(e.CurrentProgress);
+                progressBar1.Maximum = Convert.ToInt32(e.MaximumProgress);
+            }
+            catch
+            { }
+            }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab.Controls.OfType<WebBrowser>().First().Refresh();
+        }
+    }
+}
